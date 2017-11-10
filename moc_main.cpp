f83a45009424b807b676faf96bd4f1963d5f84cb
lukeahn <la393@cornell.edu>
2017-11-10 01:43:16 -0500

    merging

--- moc_main.cpp
+++ moc_main.cpp
@@ -1,7 +1,7 @@
 /****************************************************************************
 ** Meta object code from reading C++ file 'main.hh'
 **
-** Created by: The Qt Meta Object Compiler version 63 (Qt 4.8.7)
+** Created by: The Qt Meta Object Compiler version 63 (Qt 4.8.6)
 **
 ** WARNING! All changes made in this file will be lost!
 *****************************************************************************/
@@ -10,7 +10,7 @@
 #if !defined(Q_MOC_OUTPUT_REVISION)
 #error "The header file 'main.hh' doesn't include <QObject>."
 #elif Q_MOC_OUTPUT_REVISION != 63
-#error "This file was generated using the moc from 4.8.7. It"
+#error "This file was generated using the moc from 4.8.6. It"
 #error "cannot be used with the include files from this version of Qt."
 #error "(The moc has changed too much.)"
 #endif
@@ -83,7 +83,7 @@ static const uint qt_meta_data_ChatDialog[] = {
        6,       // revision
        0,       // classname
        0,    0, // classinfo
-       8,   14, // methods
+       9,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
@@ -99,6 +99,7 @@ static const uint qt_meta_data_ChatDialog[] = {
      231,  224,   11,   11, 0x0a,
      251,   11,   11,   11, 0x0a,
      272,   11,   11,   11, 0x0a,
+     292,   11,  288,   11, 0x0a,
 
        0        // eod
 };
@@ -113,6 +114,7 @@ static const char qt_meta_stringdata_ChatDialog[] = {
     "sendRumor(QString,QString,quint16)\0"
     "myPort\0sendStatus(quint16)\0"
     "processAntiEntropy()\0repeatMessage()\0"
+    "int\0pickRandomNeighbor()\0"
 };
 
 void ChatDialog::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
@@ -129,6 +131,8 @@ void ChatDialog::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, 
         case 5: _t->sendStatus((*reinterpret_cast< quint16(*)>(_a[1]))); break;
         case 6: _t->processAntiEntropy(); break;
         case 7: _t->repeatMessage(); break;
+        case 8: { int _r = _t->pickRandomNeighbor();
+            if (_a[0]) *reinterpret_cast< int*>(_a[0]) = _r; }  break;
         default: ;
         }
     }
@@ -166,9 +170,9 @@ int ChatDialog::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     if (_id < 0)
         return _id;
     if (_c == QMetaObject::InvokeMetaMethod) {
-        if (_id < 8)
+        if (_id < 9)
             qt_static_metacall(this, _c, _id, _a);
-        _id -= 8;
+        _id -= 9;
     }
     return _id;
 }
